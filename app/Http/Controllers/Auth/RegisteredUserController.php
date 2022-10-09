68beba7619c5ee7a6b18d4d690a2c3dc7b5b6fcd
Treyes <llc007.1@gmail.com>
2022-10-08 23:00:15 -0400

    Octubre22

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    //
+    public function store(Request $request){
+
+        $request->validate([
+           'name' => ['required', 'string','max:255'],
+           'email' => ['required', 'string','email','max:255','unique:users'],
+           'password' => ['required','confirmed',Rules\Password::defaults()],
+        ]);
+
+       $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => bcrypt($request->password),
+        ]);
+
+//       Auth::login($user);
+
+        return to_route('login')->with('status', 'success');
+
+    }
+}
